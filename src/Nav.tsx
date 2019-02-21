e9376dea4bec202179979d8a3666746efbb843f2
thibaultboursier <boursier.thibault@gmail.com>
2019-02-21 22:02:01 +0100

    feat(nav): add navigation bar

---
+++ src/Nav.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { createStyles, withStyles, WithStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
+
+const styles = createStyles({
+  root: {
+    flexGrow: 1
+  },
+  grow: {
+    flexGrow: 1
+  },
+  menuButton: {
+    marginLeft: -12,
+    marginRight: 20
+  }
+});
+
+export interface Props extends WithStyles<typeof styles> {}
+
+const Nav: React.StatelessComponent<Props> = ({ classes }) => (
+  <div className={classes.root}>
+    <AppBar position="static">
+      <Toolbar>
+        <IconButton
+          className={classes.menuButton}
+          color="inherit"
+          aria-label="Menu"
+        >
+          <MenuIcon />
+        </IconButton>
+        <Typography variant="h6" color="inherit" className={classes.grow}>
+          Gallery
+        </Typography>
+      </Toolbar>
+    </AppBar>
+  </div>
+);
+
+export default withStyles(styles)(Nav);
